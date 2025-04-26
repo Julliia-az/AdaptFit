95cc40fb900a96074a12fb0b9751a48ef6269cd6
maria-lucato08 <maria.lucato@etec.sp.gov.br>
2025-04-26 14:19:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+function irPagina(id) { //rola ate a pag desejada
+    const elemento = document.getElementById(id);
+    if (elemento) {
+        elemento.scrollIntoView({ behavior: "smooth" });
+    }
+    
+    bordaPreta();
+}
+
+function bordaPreta(){ //coloca a borda preta quando é clicado
+    let borda = document.getElementsByClassName("mudar-borda");
+    for(let i = 0; i < borda.length; i++){
+        borda[i].style.textShadow = `
+        1px 1px 0 black,
+        -1px -1px 0 black,
+        1px -1px 0 black,
+        -1px 1px 0 black
+    `;
+    }
+}
+
+
+
 document.getElementById('contatoForm')?.addEventListener('submit', function(event) {
     event.preventDefault(); // Evita o envio do formulário
     alert('E-mail enviado com sucesso!');
