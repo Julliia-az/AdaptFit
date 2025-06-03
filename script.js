7eee704c59b1ae3366aaec4b7745a36dae81ebc3
Julliia-az <juliagabriellyazevedo46@gmail.com>
2025-06-03 16:18:27 -0300

    Adiconei o botão de acessibilidade para pcd visual, tirei o botão 'buscar academia' e adicionei os 3 franqueados

--- script.js
+++ script.js
@@ -11,3 +11,37 @@ document.getElementById('contatoForm')?.addEventListener('submit', function(even
     alert('E-mail enviado com sucesso!');
     this.reset(); // Limpa o formulário
 });
+
+
+let modoLeituraAtivo = false;
+
+const botaoLeitor = document.getElementById('leitor');
+const icone = botaoLeitor.querySelector('img');
+
+botaoLeitor.addEventListener('click', () => {
+  modoLeituraAtivo = !modoLeituraAtivo;
+
+  botaoLeitor.style.backgroundColor = modoLeituraAtivo ? '#ddd' : '#ddd';
+
+  botaoLeitor.setAttribute('aria-label', modoLeituraAtivo ? 'Parar leitor de tela' : 'Ativar leitor de tela');
+  botaoLeitor.setAttribute('title', modoLeituraAtivo ? 'Parar leitor de tela' : 'Ativar leitor de tela');
+
+  icone.src = modoLeituraAtivo ? 'Imagens/cross.png' : 'Imagens/eye (1).png';
+});
+
+document.addEventListener('click', function(e) {
+
+  if (!modoLeituraAtivo || e.target.closest('#leitor')) return;
+
+  const texto = e.target.innerText || e.target.alt || e.target.ariaLabel;
+
+  if (texto) {
+    if (speechSynthesis.speaking) {
+      speechSynthesis.cancel();
+    }
+
+    const utterance = new SpeechSynthesisUtterance(texto);
+    utterance.lang = 'pt-BR';
+    speechSynthesis.speak(utterance);
+  }
+});
