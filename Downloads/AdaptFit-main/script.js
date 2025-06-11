521ada9df656aa5153863df90899064f7f71dede
Julia <juliagabriellyazevedo46@gmail.com>
2025-06-11 16:31:08 -0300

    Arrumando o dark mode da página inicial

---
+++ Downloads/AdaptFit-main/script.js
@@ -0,0 +1,54 @@
+function irPagina(id) { //rola ate a pag desejada
+    let elemento = document.getElementById(id);
+    if (elemento) {
+        elemento.scrollIntoView({ behavior: "smooth" });
+    }
+}
+
+
+document.getElementById('contatoForm')?.addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita o envio do formulário
+    alert('E-mail enviado com sucesso!');
+    this.reset(); // Limpa o formulário
+});
+
+
+let modoLeituraAtivo = false;
+
+const botaoLeitor = document.getElementById('leitor');
+const icone = botaoLeitor.querySelector('img');
+
+botaoLeitor.addEventListener('click', () => {
+  modoLeituraAtivo = !modoLeituraAtivo;
+
+  botaoLeitor.style.backgroundColor = modoLeituraAtivo ? '#000080' : '#000080';
+
+  botaoLeitor.setAttribute('aria-label', modoLeituraAtivo ? 'Parar leitor de tela' : 'Ativar leitor de tela');
+  botaoLeitor.setAttribute('title', modoLeituraAtivo ? 'Parar leitor de tela' : 'Ativar leitor de tela');
+
+  icone.src = modoLeituraAtivo ? 'Imagens/x.png' : 'Imagens/olho.png';
+});
+
+document.addEventListener('click', function(e) {
+
+  if (!modoLeituraAtivo || e.target.closest('#leitor')) return;
+
+  const texto = e.target.innerText || e.target.alt || e.target.ariaLabel;
+
+  if (texto) {
+    if (speechSynthesis.speaking) {
+      speechSynthesis.cancel();
+    }
+
+    const utterance = new SpeechSynthesisUtterance(texto);
+    utterance.lang = 'pt-BR';
+    speechSynthesis.speak(utterance);
+  }
+});
+
+//dark mode
+const chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
+  document.body.classList.toggle('dark');
+});
